fc298db1b6d222741fb2cdbbe6cefa7ef601a745
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-07-23 19:00:10 +0200

    customising django

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import Product, Category
 
+''' 
+    This snippet is for customising django
+    admin to make it more user friendly.
+'''
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
 
+    ordering = ('sku',)
 
-admin.site.register(Product)
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
