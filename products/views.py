7622744f0721a1dcbbad8ba9c023e69e6a6ea89c
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-07-28 08:33:15 +0200

    Added current categories to template and styled buttons

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
+
 
 
 """ A view to show all products """
@@ -7,9 +8,17 @@ from .models import Product
 def all_products(request):
     
     products = Product.objects.all()
+    categories = None
+
+
+    if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
