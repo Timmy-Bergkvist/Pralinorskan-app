2e8cf5625e7001d17ff5f1becaa7335bcdca5b40
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-08-03 13:15:29 +0200

    Added urls, views, template and requirements to checkout app

--- pralinorskan_app/settings.py
+++ pralinorskan_app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cart',
     'checkout',
     'profiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'pralinorskan_app.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +79,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
