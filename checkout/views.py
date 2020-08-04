b0727a97ee246d975bf8fbb208fcb663e335e966
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-08-04 16:00:52 +0200

    Setting up stripe payment

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
