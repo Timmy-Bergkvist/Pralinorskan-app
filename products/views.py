58bc01e62049f6cc43fc021eccd9e0a6a7fefd9d
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-08-13 14:11:32 +0200

    Added paginations to products page and functions in products views file

--- products/views.py
+++ products/views.py
@@ -1,19 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product, Category
-
 
 
 """ A view to show all products """
 
 def all_products(request):
     
-    products = Product.objects.all()
+    product_list = Product.objects.all()
+    # Creates pagination
+    page = request.GET.get('page', 1)
+    paginator = Paginator(product_list, 12) # Show 12 products per page
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+        
     categories = None
-
-
     if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
+            products = product_list.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
     context = {
