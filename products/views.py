bef91c575267e75097d3bae030ac88cc02458fa2
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-08-17 07:23:54 +0200

    Added functions for sorting and paginations. Removed unnecessary requirements.txt

--- products/views.py
+++ products/views.py
@@ -4,39 +4,65 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product, Category
 
 
-""" A view to show all products """
-
 def all_products(request):
-    
+    """ A view to show all products, including sorting.
+        Pagination included for more than 8 products on page.
+    """
+
     product_list = Product.objects.all()
+
+    categories = None
+    sort = None
+    direction = None
+    collection_page = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                product_list = product_list.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            product_list = product_list.order_by(sortkey)
+
+        if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                product_list = Product.objects.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
+                collection_page = request.GET['category']
+
+    current_sorting = f'{sort}_{direction}'
     # Creates pagination
-    page = request.GET.get('page', 1)
-    paginator = Paginator(product_list, 8) # Show 8 products per page
+    page = request.GET.get('page')
+    paginator = Paginator(product_list, 8)  # Show 8 products per page
+
     try:
         products = paginator.page(page)
     except PageNotAnInteger:
         products = paginator.page(1)
     except EmptyPage:
         products = paginator.page(paginator.num_pages)
-        
-    categories = None
-    if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = product_list.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
         'current_categories': categories,
+        'current_sorting': current_sorting,
+        'collection_page': collection_page,
     }
 
     return render(request, 'products/products.html', context)
 
 
-
-""" A view to show individual product details """
-
 def product_detail(request, product_id):
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
