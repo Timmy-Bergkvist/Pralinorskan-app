2bba98ae19ac48e8294b06a310fa27eab5a76a9b
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-08-18 19:46:41 +0200

    Added localhost to allowed host and added webhook secret key

--- pralinorskan_app/settings.py
+++ pralinorskan_app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['pralinorskan-app.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['pralinorskan-app.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'pralinorskan_app.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL',''))
     }
 else:
     DATABASES = {
