e155cfac30019f15a3adcca9da29115a1f16361d
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-08-18 17:05:44 +0200

    Deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pralinorskan-app.wsgi:application
--- pralinorskan_app/settings.py
+++ pralinorskan_app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pralinorskan-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -122,12 +122,18 @@ WSGI_APPLICATION = 'pralinorskan_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
