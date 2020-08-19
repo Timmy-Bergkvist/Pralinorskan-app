a22577904b0b2f5730c0a62fdcc7d9dcada29631
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-08-19 15:43:42 +0200

    Added CacheControl

--- pralinorskan_app/settings.py
+++ pralinorskan_app/settings.py
@@ -181,13 +181,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
-   
+   # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'pralinorskan-app'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    
+
     # F string so my bucket name from above will be interpreted and added to generate the appropriate URL
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
